7cdb6b7302ea8ab096d059dc151e1500412be9ca
narmpiyanuch <a.piyanuch@hotmail.com>
2025-02-28 18:34:21 +0700

    create-covid-r4-summary

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,113 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+import axios from 'axios';
+import Chart from 'react-apexcharts'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([]);
+  const [count, setCount] = useState(null);
+  const [ageRange, setAgeRange] = useState({});
+  const [province, setProvince] = useState({});
+  const [chartData, setChartData] = useState({})
+
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await axios.get('https://covid19.ddc.moph.go.th/api/Deaths/round-4-line-list')
+        setData(response?.data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getData();
+  }, []);
+
+  useEffect(() => {
+    if (data && data.data) {
+      // set total count
+      setCount(data.data.length);
+
+      // set age range count
+      const ageRangeData = data.data.reduce((acc, item) => {
+        if (item.age_range) {
+          acc[item.age_range] = (acc[item.age_range] || 0) + 1;
+        }
+        return acc;
+      }, {})
+      setAgeRange(ageRangeData)
+
+      // set province count
+      const provinceData = data.data.reduce((acc, item) => {
+        if (item.province) {
+          acc[item.province] = (acc[item.province] || 0) + 1;
+        }
+        return acc
+      }, {})
+      setProvince(provinceData)
+    }
+  }, [data])
+
+  useEffect(() => {
+    if (province && Object.keys(province).length > 0) {
+      const provinceNames = Object.keys(province);
+      const provinceCounts = Object.values(province);
+
+      const chartOptions = {
+        chart: {
+          id: 'province-chart',
+        },
+        labels: provinceNames,
+        dataLabels: {
+          enabled: true,
+        },
+        title: {
+          text: 'จำนวนผู้ป่วยยืนยันตามจังหวัด',
+          align: 'center',
+        },
+      };
+
+      setChartData({
+        options: chartOptions,
+        series: provinceCounts,
+      });
+    }
+  }, [province]);
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <h2>Covid round 4 summary</h2>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '180px', border: '1px solid gray', borderRadius: '6px', padding: '10px' }}>
+        <div style={{ fontWeight: 'bold', fontSize: '20px' }}>Total of deaths</div>
+        <div style={{ marginTop: '10px' }}>{count}</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div style={{ display: 'flex', flexDirection: 'row', marginTop: '20px', gap: '40px' }}>
+        <div style={{ width: '200px' }}>
+          <table>
+            <thead>
+              <tr>
+                <th>Age Range</th>
+                <th>Count</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Object.entries(ageRange).map(([ageRange, count]) => (
+                <tr key={ageRange}>
+                  <td>{ageRange}</td>
+                  <td>{count}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <div style={{ width: '600px' }}>
+          {chartData?.options && chartData?.series && (
+            <Chart options={chartData.options} series={chartData.series} type='pie' />
+          )}
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
